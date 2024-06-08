479ca70da63ec4078e05d28bf916b58568ab3690
atusy <30277794+atusy@users.noreply.github.com>
2024-06-08 22:00:02 +0900

    tutorial(remix): validating params and throwing responses

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,16 +1,22 @@
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
   return json({ contact });
 };
 
 export default function Contact() {
   const { contact } = useLoaderData<typeof loader>();
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
 
   return (
     <div id="contact">
