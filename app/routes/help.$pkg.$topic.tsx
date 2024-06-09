a08572506451d1de00403eac043d2fbf871de283
atusy <30277794+atusy@users.noreply.github.com>
2024-06-09 15:22:58 +0900

    feat(fuzzyhelp): show HTML help

--- app/routes/help.$pkg.$topic.tsx
+++ app/routes/help.$pkg.$topic.tsx
@@ -1,3 +1,4 @@
+import "./help.css"
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useOutletContext } from "@remix-run/react";
 import { json } from "@remix-run/node";
@@ -6,30 +7,44 @@ import {
 } from "@remix-run/react";
 import { FzfResultItem } from "fzf";
 import invariant from "tiny-invariant";
+import { WebR } from 'webr';
+
+let webR = new WebR();
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   invariant(params.pkg, "Missing pkg param");
   invariant(params.topic, "Missing topic param");
-  return json({ pkg: params.pkg, topic: params.topic })
+  await webR.init();
+  const { topic, pkg } = params
+  const shelter = await new webR.Shelter();
+  const tick = "`"
+  const result = await shelter.captureR(`
+    x <- help(${tick}${topic}${tick}, package = ${pkg}, help_type = "html")
+    paths <- as.character(x)
+    file <- paths[1L]
+    pkgname <- basename(dirname(dirname(file)))
+    try({
+      helpfile <- utils:::.getHelpFile(file)
+      tools::Rd2HTML(helpfile, package = pkgname)
+    }, silent = TRUE)
+  `);
+  const help = result.output;
+  shelter.purge()
+  return json({ help })
 }
 
 export default function Help() {
-  const { pkg, topic } = useLoaderData<typeof loader>();
-  const ctx = useOutletContext();
-  const toc = ctx.toc as FzfResultItem<Record<string, string>>[]
-  const webR = ctx.webR
+  const { help } = useLoaderData<typeof loader>();
+  const ctx = useOutletContext() as {
+    toc: FzfResultItem<Record<string, string>>[]
+    webR: WebR
+  };
+  webR = webR // somehow, error happens without this...
+  webR = ctx.webR
   return (
-    <div id="contact">
-      <p>
-        {
-          toc.map(({ item }) => {
-            if (item.pkg === pkg && item.topic === topic) {
-              return item.title
-            }
-            return ""
-          })
-        }
-      </p>
-    </div>
+    <iframe
+      srcDoc={help.map((x) => x.data).join("\n")}
+      id="helpContent"
+    ></iframe>
   );
 }
